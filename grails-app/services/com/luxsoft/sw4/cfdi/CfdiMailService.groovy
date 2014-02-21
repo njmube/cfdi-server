55e92846383bc9672911d98565ca43085888d681
Ruben Cancino <rubencancino6@gmail.com>
2014-02-20 19:16:29 -0600

    Ajustes varios 2

--- grails-app/services/com/luxsoft/sw4/cfdi/CfdiMailService.groovy
+++ grails-app/services/com/luxsoft/sw4/cfdi/CfdiMailService.groovy
@@ -86,7 +86,7 @@ class CfdiMailService {
              ByteArrayInputStream is=new ByteArrayInputStream(v)
              ComprobanteDocument docto=ComprobanteDocument.Factory.parse(is)
              Comprobante comprobante=docto.comprobante
-             byte[] pdf=pdfGenerator.generar(comprobante)
+             byte[] pdf=pdfGenerator.generar(comprobante,obtenerParametrosComplementarios(null))
              pdfFiles[k.replaceAll('.xml','.pdf')]=pdf
         }
         /*
@@ -107,6 +107,31 @@ class CfdiMailService {
         */
        
     }
+	
+	def obtenerParametrosComplementarios(String cargoId){
+		String select="""
+			SELECT V.CARGO_ID,V.CLAVE AS CLAVCTE,V.NOMBRE
+			,CONCAT(IFNULL((SELECT CONCAT('1) ',T.TELEFONO)  FROM sx_clientes_tels T WHERE T.CLIENTE_ID=V.CLIENTE_ID AND T.TIPO='TEL1'),' ')
+			,IFNULL((SELECT CONCAT('2) ',T.TELEFONO)  FROM sx_clientes_tels T WHERE T.CLIENTE_ID=V.CLIENTE_ID AND T.TIPO='TEL2'),' ')
+			,IFNULL((SELECT CONCAT('3) ',T.TELEFONO)  FROM sx_clientes_tels T WHERE T.CLIENTE_ID=V.CLIENTE_ID AND T.TIPO='TEL3'),' ')  
+			,IFNULL((SELECT CONCAT('4) ',T.TELEFONO)  FROM sx_clientes_tels T WHERE T.CLIENTE_ID=V.CLIENTE_ID AND T.TIPO='TEL4'),' ') 
+			,IFNULL((SELECT CONCAT('CEL ',T.TELEFONO)  FROM sx_clientes_tels T WHERE T.CLIENTE_ID=V.CLIENTE_ID AND T.TIPO='CEL'),' ')	
+			,IFNULL((SELECT CONCAT('FAX ',T.TELEFONO)  FROM sx_clientes_tels T WHERE T.CLIENTE_ID=V.CLIENTE_ID AND T.TIPO='FAX'),' ')
+			) AS TEL
+			,V.DIA_DE_REV AS D_REV,V.DIA_DEL_PAGO AS D_PAG,V.COBRADOR_ID AS COB,V.VENDEDOR_ID AS VEND,V.PLAZO,(CASE WHEN V.REVISION IS TRUE THEN 'R' ELSE 'F' END) AS FREV
+           ,V.PEDIDO_FENTREGA AS ENVIO,(CASE WHEN V.ORIGEN='CRE' THEN 'CREDITO' ELSE 'CONTADO' END) AS TIPO,V.COMENTARIO,V.INSTRUCCION_ENTREGA AS DIR_ENTREGA,V.PUESTO
+           ,(SELECT S.NOMBRE FROM sx_socios S WHERE S.SOCIO_ID=V.SOCIO_ID) AS SOCIO
+           ,V.PEDIDO_FOLIO AS PEDIDO,V.MODIFICADO_USERID AS ELAB_VTA,V.KILOS,V.SURTIDOR,V.CREADO_USERID AS ELAB_FAC,V.PEDIDO_IP_CREATED AS IP
+           ,(SELECT S.CLAVE FROM sw_sucursales S WHERE S.SUCURSAL_ID=V.SUCURSAL_ID) AS SUC
+           ,V.CE AS PCE,V.FPAGO
+          ,V.MONEDA
+          FROM SX_VENTAS V 
+          WHERE V.CARGO_ID=?
+		"""
+		Sql sql=new Sql(dataSource_importacion)
+		def row=sql.firstRow(select,[cargoId])
+		return row
+	}
     
     def testPdf(){
         def id='00000000-43120afd-0143-12919ac1-002d'
@@ -115,7 +140,10 @@ class CfdiMailService {
         ByteArrayInputStream is=new ByteArrayInputStream(row.xml)
         ComprobanteDocument docto=ComprobanteDocument.Factory.parse(is)
         Comprobante comprobante=docto.comprobante
-        byte[] pdf=pdfGenerator.generar(comprobante)
+		//println 'Buscando parametros complementarios para CARGO_ID: '+row.ORIGEN_ID
+		def complemento=obtenerParametrosComplementarios(row.ORIGEN_ID)
+		//println 'Complemento: '+complemento
+        byte[] pdf=pdfGenerator.generar(comprobante,complemento)
         return pdf
         
     }
--- src/groovy/com/luxsoft/sw4/cfdi/PdfGenerator.groovy
+++ src/groovy/com/luxsoft/sw4/cfdi/PdfGenerator.groovy
@@ -6,6 +6,8 @@
 
 package com.luxsoft.sw4.cfdi
 
+import mx.gob.sat.cfd.x3.TUbicacion;
+
 import org.springframework.context.ResourceLoaderAware
 import org.springframework.core.io.Resource
 import org.springframework.core.io.ResourceLoader
@@ -14,6 +16,8 @@ import java.text.MessageFormat
 import javax.imageio.ImageIO
 import org.codehaus.groovy.grails.plugins.jasper.JasperExportFormat
 import org.codehaus.groovy.grails.plugins.jasper.JasperReportDef
+
+import mx.gob.sat.cfd.x3.ComprobanteDocument.Comprobante
 
 /**
  *
@@ -25,7 +29,7 @@ class PdfGenerator implements ResourceLoaderAware{
     
     def jasperService
     
-    byte[] generar(comprobante){
+    byte[] generar(comprobante,complemento){
         def conceptos=comprobante.getConceptos().getConceptoArray()
         def modelData=conceptos.collect { cc ->
             def res=[
@@ -47,23 +51,91 @@ class PdfGenerator implements ResourceLoaderAware{
             return res
         }
         JasperReportDef reportDef=new JasperReportDef(
-            name:'CfdiFactura.jrxml',
+            name:'FacturaCFDI.jrxml',
             fileFormat:JasperExportFormat.PDF_FORMAT,
             reportData:modelData,
-            parameters:resolverParametros(comprobante)
+            parameters:resolverParametros(comprobante,complemento)
         )
         ByteArrayOutputStream out=jasperService.generateReport(reportDef)
         return out.toByteArray()
         
     }
     
-    Map resolverParametros(comprobante){
+    Map resolverParametros(Comprobante comprobante,complemento){
         def map=[:]
-        Resource rs=resourceLoader.getResource("images/companyLogo.jpg")
+       
+		 Resource rs=resourceLoader.getResource("images/empresaFacLogo.jpg")
         Image logo=ImageIO.read(rs.getInputStream())
         println 'Logo cargado: '+logo
-        map['LOGO',logo]
-        
+        map['LOGO_EMPRESA']=logo
+		
+		
+		Image img2=ImageIO.read(resourceLoader.getResource("images/facUSD.jpg").getInputStream())
+		println 'Image 2:'+img2
+		map['FAC_USD']=img2
+		
+		println 'Procesando complemento: '+complemento
+		map['CLAVCTE']=complemento.CLAVCTE
+		map['NOMBRE']=complemento.NOMBRE
+        map.TEL=complemento.TEL
+		
+		
+		map.CLAVCTE=complemento.CLAVCTE
+		map.TEL=complemento.TEL
+		map.D_REV=complemento.D_REV
+		map.D_PAG=complemento.D_PAG
+		map.COB=complemento.COB
+		map.VEND=complemento.VEND
+		map.PLAZO=complemento.PLAZO
+		map.FREV=complemento.FREV
+		map.ENVIO=complemento.ENVIO
+		map.TIPO=complemento.TIPO
+		map.COMENTARIO=complemento.COMENTARIO
+		map.DIR_ENTREGA=complemento.DIR_ENTREGA
+		map.PUESTO=complemento?.PUESTO?'***PUESTO***':''
+		map.SOCIO=complemento.SOCIO
+		map.PEDIDO=complemento.PEDIDO
+		map.ELAB_VTA=complemento.ELAB_VTA
+		map.KILOS=complemento.KILOS
+		map.SURTIDOR=complemento.SURTIDOR
+		map.ELAB_FAC=complemento.ELAB_FAC
+		map.IP=complemento.IP
+		map.SUC=complemento.SUC
+		map.PCE=complemento.PCE?'COD':''
+		map.FPAGO=complemento.FPAGO
+		map.MONEDA=complemento.MONEDA
+		map.CARGO_ID=complemento.CARGO_ID
+		
+		//Emisor emisor=comprobante.emisor
+		map.EMISIOR=comprobante.emisor.nombre
+		map["EMISOR_RFC"]=comprobante.emisor.rfc
+		map.DIRECCION_EMISOR=getDireccionEnFormatoEstandar(comprobante.emisor.getDomicilioFiscal())
+		//map.REGIMEN=comprobante.emisor.getRegimenFiscalArray().class
+		map.REGIMEN='A DONDE ?'
+		
+		map.FECHA=comprobante.fecha.getTime()
+		map.CONDICIONES_DE_PAGO=comprobante.getMetodoDePago()
+		map.METODO_PAGO=comprobante.getFormaDePago()
+		
+		map.NFISCAL=comprobante.serie+"-"+comprobante.folio
+		map.TIPO_CFDI=comprobante.getTipoDeComprobante()
+		map.NUM_CERTIFICADO=comprobante.getNoCertificado()
+		map.SELLO_DIGITAL=comprobante.getSello()
+		//map.LEYENDA DE CFDI=comprobante.LEYENDA DE CFDI
+		//map.EXPEDIDO_DIRECCION=comprobante.EXPEDIDO_DIRECCION
+		map.CUENTA=comprobante.getNumCtaPago()
+		
+		
+		
+		
+		map.RFC=comprobante.receptor.rfc
+		map.DIRECCION=getDireccionEnFormatoEstandar(comprobante.receptor.getDomicilio())
+		
+		//map.COMENTARIO_FAC=comprobante.COMENTARIO_FAC
+		map.IMP_CON_LETRA=ImporteALetra.aLetra(comprobante.total)
+		map.PINT_IVA=MonedaUtils.IVA*100
+		
+		
         //QRCode
         map["QR_CODE"]= QRCodeUtils.generarQR(comprobante)
         TimbreFiscal timbre=new TimbreFiscal(comprobante)
@@ -75,6 +147,24 @@ class PdfGenerator implements ResourceLoaderAware{
             
         return map
     }
+	
+	String getDireccionEnFormatoEstandar(def u){
+		String pattern="{0} {1} {2} {3}" +
+				" {4} {5} {6}" +
+				" {7} {8}";
+		//StringUtils.
+		return MessageFormat.format(pattern
+			,u.getCalle() !=null?u.getCalle():""
+			,(u.getNoExterior()!=null && !u.getNoExterior().equals(".") )?"NO."+u.getNoExterior():""
+			,(u.getNoInterior()!=null && !u.getNoInterior().equals(".") )?"INT."+u.getNoInterior():""
+			,u.getColonia()!=null?","+u.getColonia():""
+			,u.getCodigoPostal() !=null?","+u.getCodigoPostal():""
+			,u.getMunicipio()!=null?","+u.getMunicipio():""
+			,u.getLocalidad()!=null?","+u.getLocalidad():""
+			,u.getEstado()!=null?","+u.getEstado()+",":""
+			,u.getPais()!=null?u.getPais():""
+		);
+	}
     
     void setResourceLoader(ResourceLoader resourceLoader){
         this.resourceLoader=resourceLoader
